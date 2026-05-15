e091923497b221ca0828592fec0a6eea1f404619
Zac <audreyy.zac@gmail.com>
2026-05-16 02:07:58 +0800

    Official Push

---
+++ src/app.jsx
@@ -0,0 +1,12 @@
+// src/App.jsx
+import LandingPage from './components/LandingPage'
+
+function App() {
+  return (
+    <div className="min-h-screen">
+      <LandingPage />
+    </div>
+  )
+}
+
+export default App
