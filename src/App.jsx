83a0843e7de11890b7ce3e3ccd59a265e2640a92
Zac <audreyy.zac@gmail.com>
2026-05-16 10:50:28 +0800

    Backend start

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
 // src/App.jsx
+
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+
 import LandingPage from './components/LandingPage'
+import Login from './pages/Login'
+import AdminDashboard from './pages/AdminDashboard'
 
 function App() {
   return (
-    <div className="min-h-screen">
-      <LandingPage />
-    </div>
+    <BrowserRouter>
+      <div className="min-h-screen">
+        <Routes>
+          {/* Public Website */}
+          <Route path="/" element={<LandingPage />} />
+
+          {/* Auth */}
+          <Route path="/login" element={<Login />} />
+
+          {/* Admin */}
+          <Route
+            path="/admin/dashboard"
+            element={<AdminDashboard />}
+          />
+        </Routes>
+      </div>
+    </BrowserRouter>
   )
 }
 
---
+++ src/pages/AdminDashboard.jsx
@@ -0,0 +1,11 @@
+function AdminDashboard() {
+  return (
+    <div className="min-h-screen p-8">
+      <h1 className="text-3xl font-bold">
+        Admin Dashboard
+      </h1>
+    </div>
+  )
+}
+
+export default AdminDashboard
