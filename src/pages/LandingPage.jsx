7c86dd9cd266ddff583590a787847453392a8882
Zac <audreyy.zac@gmail.com>
2026-05-17 01:43:57 +0800

    adjust transparency of background

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -327,7 +327,7 @@ export default function LandingPage() {
       backgroundRepeat: "no-repeat",
       backgroundPosition: "right center",
       backgroundSize: "auto 80%", // Adjust size as needed
-      opacity: 0.12,              // Very transparent
+      opacity: 0.10,              // Very transparent
       right: "-15%",              // Shift to the right, cutting half
       top: "50%",
       transform: "translateY(-50%)",
